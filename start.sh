888fedd4f487e8f07f0b1b0c38c221e2843876f2
settrace <comsgn@gmail.com>
2024-06-09 23:12:56 +0000

    fix2

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ex
-git pull origin master -f
+git fetch origin master
+git checkout origin/master -f
 mkdir -p /models
 huggingface-cli download -hfr microsoft/Phi-3-mini-4k-instruct-gguf -hff Phi-3-mini-4k-instruct-q4.gguf --local-dir ./models --resume-download
 exec python3 ./src/handler.py

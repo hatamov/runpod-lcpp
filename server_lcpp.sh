8fda7aa71caf802e8d72369177c1ad0cd1c64fac
Farkhad Khatamov <farkhad.khatamov@reddit.com>
2024-06-14 01:19:37 -0400

    iter

--- server_lcpp.sh
+++ server_lcpp.sh
@@ -7,7 +7,7 @@ HFF="${HFF:-Phi-3-mini-4k-instruct-q4.gguf}"
 HFR_LOCAL_PATH="$MODELS_DIR/${HFR_LOCAL_PATH:-$HFR}"
 ./snapshot_download.py --repo_id "$HFR" --local_dir "$HFR_LOCAL_PATH" --allow_patterns "$HFF"
 
-MODEL_FILE="$HFR_LOCAL_PATH/${MODEL_FILE:-$HFF}"
+MODEL_FILE="${MODEL_FILE:-$HFR_LOCAL_PATH/$HFF}"
 
 python3 -m llama_cpp.server --host 127.0.0.1 --port $S_LCPP_PORT --model "$MODEL_FILE" \
     --n_gpu_layers -1 --flash_attn true \
